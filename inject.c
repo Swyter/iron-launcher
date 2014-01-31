7852fe545cbd92910b40e1e1e5522b9aab60674f
Swyter <690187+Swyter@users.noreply.github.com>
2014-01-31 17:47:12 +0100

    *refactor and cleanup.

--- inject.c
+++ inject.c
@@ -1,30 +1,42 @@
 #include <windows.h>
 #include <stdio.h>
 
-/* Global variables */
-HINSTANCE dll_handle;
-HINSTANCE org_handle;
+#define E_FAIL 0x80004005
 
-char dllpath[MAX_PATH];
+/* Global variables */
+HINSTANCE self_handle;
+HINSTANCE orig_handle;
+
+char orig_path[MAX_PATH];
 
 
 int __stdcall DirectInput8Create(int a1, int a2, int a3, int a4, int a5)
 {
   int result; // eax@3
   FARPROC v6; // [sp+0h] [bp-114h]@2
-  char LibFileName[MAX_PATH] = {0}; // [sp+4h] [bp-110h]@1
   char v8; // [sp+5h] [bp-10Fh]@1
   unsigned int v9; // [sp+10Ch] [bp-8h]@1
   HMODULE hModule; // [sp+110h] [bp-4h]@1
   int v11; // [sp+114h] [bp+0h]@1
 
-  GetSystemDirectoryA(&LibFileName, 0x104u);
-  strcat(&LibFileName, "\\dinput8.dll");
-  hModule = LoadLibraryA(&LibFileName);
-  if ( hModule && (v6 = GetProcAddress(hModule, "DirectInput8Create")) != 0 )
+  GetSystemDirectoryA(&orig_path, 0x104u); strcat(&orig_path, "\\dinput8.dll");
+
+  orig_handle = LoadLibraryA(&orig_path);
+  
+  
+  
+  if (orig_handle && (v6 = GetProcAddress(orig_handle, "DirectInput8Create")) != 0 )
     result = ((int (__stdcall *)(int, int, int, int, int))v6)(a1, a2, a3, a4, a5);
+    
   else
-    result = -2147467259;
+    result = E_FAIL;
+    
+    
+  char msg[MAX_PATH]; sprintf(msg,"hola: %x/%x/%x/%x/%p/%x/  %p/%p  %x",
+                              result, a1, a2, a3, a4, a4, v6, orig_handle, self_handle);
+  
+  MessageBoxA(0, msg, orig_path, 0);
+
   return result;
 }
 
@@ -42,14 +54,15 @@ BOOL __stdcall DllMain(
       /* We don't need the thread based attach/detach messages in this DLL */
       DisableThreadLibraryCalls(hModule);
       
-      dll_handle = hModule;
-      MessageBoxA(0, "loaded...", "lol", 0);
+      self_handle = hModule;
+      
+      MessageBoxA(0, "loaded...", orig_path, 0);
       break;
 
     /* If the process ask for unloading */
     case DLL_PROCESS_DETACH:
     
-      MessageBoxA(0, "soltando la pechuga!", dllpath, 0);
+      MessageBoxA(0, "unloading!", orig_path, 0);
       break;
   }
 
