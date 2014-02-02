4e123b174e8314ae64f036c83ea3982cd9986d4c
Swyter <Swyter@Portatil>
2014-02-02 21:03:47 +0100

    *cleaned up a bit more, removing and re-formatting the logging code. *remove a bit more of cruft and unused stuff, its in hg, so shouldn't be a problem.

--- inject.c
+++ inject.c
@@ -25,7 +25,7 @@ BOOL FileExists(LPCTSTR szPath)
   DWORD dwAttrib = GetFileAttributes(szPath);
 
   return (dwAttrib != INVALID_FILE_ATTRIBUTES && 
-         !(dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
+        !(dwAttrib  & FILE_ATTRIBUTE_DIRECTORY));
 }
 
 
@@ -40,9 +40,9 @@ HANDLE __stdcall il_CreateFile(
 )
 {
 
-  //char cosa[500]; sprintf(cosa,"CreateFile called! (lpFileName=%s,dwDesiredAccess=%x,dwShareMode=%x)",
-  //                        lpFileName, dwDesiredAccess, dwShareMode);
-  //il_log(WARN,cosa);
+  char debug[500]; sprintf(debug,"CreateFile called! (lpFileName=%s,dwDesiredAccess=%x,dwShareMode=%x)",
+                           lpFileName, dwDesiredAccess, dwShareMode);
+  il_log(INFO,debug);
   
   static struct{const char *src; const char *dst;} search_locations[] =
   {
@@ -66,16 +66,12 @@ HANDLE __stdcall il_CreateFile(
     { "CommonRes\\core_pictures.brf",  "Modules\\%s\\Data\\core_pictures.brf"   },
     { NULL, NULL }
   };
-  
-   //MessageBoxA(0, search_locations[1].src, "match", 0);
-   //MessageBoxA(0, search_locations[1].dst, "match", 0);
-   
+     
   char *mod_name = get_current_mod_name();
-   
+  
   int i;
   for(i=0;i<sizeof(search_locations);i++)
   { 
-   // MessageBoxA(0, search_locations[i].src, "match", 0);
     if (search_locations[i].src == 0) break;
   
     //if(strncmp(lpFileName, search_locations[i].src, sizeof(search_locations[i].src))==0)
@@ -83,77 +79,25 @@ HANDLE __stdcall il_CreateFile(
     {
       char modded[300];
       sprintf(modded, search_locations[i].dst, mod_name);
-      il_log(ERRO, "Found modular replacement match! as follows:");
-      il_log(ERRO, lpFileName);
+      
+      char debug[400]; sprintf(debug,  "  |  cool, found a modular replacement candidate: %s", lpFileName);
+      il_log(WARN,debug);
       
       if(FileExists(modded))
       {
-        MessageBoxA(0, modded, "match", 0);
         lpFileName = modded;
-        il_log(ERRO, modded);
+        
+        MessageBoxA(0, modded, "match", 0);
+        sprintf(debug,  "  |    the replacement file exists, replacing by: %s", modded);
+        il_log(WARN,debug);
+      }
+      
+      else
+      {
+        il_log(ERRO, "  |    the replacement file doesn't seem to exist in the mod... :(");
       }
     }
   }
-  
-  // char *p = search_locations[0];
-  
-  // while(1)
-  // {
-  // if (*p->src == 0) break;
-  
-      // MessageBoxA(0, p->src, "match", 0);
-      // p++;
-  // }
-  
-  
-  // int i;
-  // for(i=0;i<sizeof(search_locations)-2;i++)
-  // { 
-    // MessageBoxA(0, search_locations[i].src, "match", 0);
-
-  
-    // if(strncmp(lpFileName, search_locations[i].src, sizeof(search_locations[i].src))==0)
-    // {
-      // char modded[300];
-      // sprintf(modded, search_locations[i].dst, get_current_mod_name());
-      // il_log(ERRO, "Found modular replacement match! as follows:");
-      // il_log(ERRO, modded);
-      
-      // lpFileName = modded;
-    // }
-  // }
-  
-  // if(strcmp(lpFileName,"Data\\font_data.xml")==0)
-  // {
-    // lpFileName = "modules\\tld-svn\\Data\\font_data.xml";
-  // }
-  // if(strcmp(lpFileName,"Data\\skeleton_bodies.xml")==0)
-  // {
-    // lpFileName = "modules\\tld-svn\\Data\\skeleton_bodies.xml";
-  // }
-  // if(strcmp(lpFileName,"Data\\flora_kinds.txt")==0)
-  // {
-    // lpFileName = "modules\\tld-svn\\Data\\flora_kinds.txt";
-  // }
-  
-  // if(strcmp(lpFileName,"CommonRes\\core_shaders.brf")==0)
-  // {
-    // lpFileName = "modules\\tld-svn\\Data\\core_shaders.brf";
-  // }
-  // if(strcmp(lpFileName,"CommonRes\\core_textures.brf")==0)
-  // {
-    // lpFileName = "modules\\tld-svn\\Data\\core_textures.brf";
-  // }
-  // if(strcmp(lpFileName,"CommonRes\\core_materials.brf")==0)
-  // {
-    // lpFileName = "modules\\tld-svn\\Data\\core_materials.brf";
-  // }
-  // if(strcmp(lpFileName,"CommonRes\\core_ui_meshes.brf")==0)
-  // {
-    // lpFileName = "modules\\tld-svn\\Data\\core_ui_meshes.brf";
-  // }
-
-  
 
   return CreateFile(
             lpFileName,
@@ -166,41 +110,10 @@ HANDLE __stdcall il_CreateFile(
   );
 }
 
-BOOL __stdcall il_ReadFile(
-  HANDLE hFile,
-  LPVOID lpBuffer,
-  DWORD nNumberOfBytesToRead,
-  LPDWORD lpNumberOfBytesRead,
-  LPOVERLAPPED lpOverlapped
-)
-{
-  //char cosa[500]; sprintf(cosa,"ReadFile called! (hFile=%x,lpBuffer=%x,nNumberOfBytesToRead=%x,lpNumberOfBytesRead=%x,lpOverlapped=%x)",
-  //                        hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
-  //il_log(WARN,cosa);
-  //MessageBoxA(0, "called!", "match", 0);
-  return ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesRead, lpOverlapped);
-}
-
-
-
 void il_configure_hooks(void)
 {
   il_log(INFO, "thread started");
   
-  
-   // IMAGE_DOS_HEADER* pIDH = (IMAGE_DOS_HEADER*)GetModuleHandle( NULL ); 
-   // IMAGE_NT_HEADERS* pINH = (IMAGE_NT_HEADERS*)((BYTE*)pIDH + (pIDH -> e_lfanew)); 
-   // IMAGE_OPTIONAL_HEADER IOH = pINH -> OptionalHeader; 
-  // char coso[4000];
-
-   // sprintf(coso, "Magic number is : %u\n", pIDH -> e_magic ); 
-     // MessageBoxA(0, coso, "match", 0);
-
-   // sprintf(coso, "Address of entry point is : %#x", IOH.AddressOfEntryPoint );  
-   
-  // MessageBoxA(0, coso, "match", 0);
-
-   
   IMAGE_DOS_HEADER *dos_header = (PIMAGE_DOS_HEADER)GetModuleHandle(NULL); //0x400000;
   IMAGE_NT_HEADERS *nt_header  = (PIMAGE_NT_HEADERS)((BYTE*)dos_header + (dos_header->e_lfanew));
   IMAGE_OPTIONAL_HEADER IOH    = nt_header -> OptionalHeader;
@@ -209,22 +122,27 @@ void il_configure_hooks(void)
 
   if (imp==0)
   {
-    il_log(ERRO,"looks like the parent exe doesn't have an import table, bailing out...");
+    il_log(ERRO,"looks like the parent exe doesn't have an import table, bailing out... :(");
     return;
   }
   
-  //char cosa[50];
-  //sprintf(cosa,"image base: %x, import virtualaddr: %x first thunk: %x", nt_header-> OptionalHeader.ImageBase, nt_header-> OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress, imp->FirstThunk);
-  //MessageBoxA(0, cosa, "match", 0);
+  char debug[50];
+  sprintf(debug, "image base: %x, import virtualaddr: %x first thunk: %x",
+                  nt_header-> OptionalHeader.ImageBase,
+                  nt_header-> OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress,
+                  imp->FirstThunk
+  );
+  
+  il_log(ERRO, debug);
 
   while(1)
   {
     if (imp->OriginalFirstThunk == 0) break;
     
-    //sprintf(cosa,"import name: %s, first thunk: %x", (BYTE*)dos_header + imp->Name, imp->FirstThunk);
-    //MessageBoxA(0, cosa, "match", 0);
-    
-    //il_log(INFO, cosa);
+    sprintf(debug,"import name: %s, first thunk: %x",
+                  (BYTE*)dos_header + imp->Name, imp->FirstThunk
+    );
+    il_log(INFO, debug);
     
     IMAGE_THUNK_DATA *imp_name_table = (PIMAGE_THUNK_DATA)((BYTE*)dos_header + (imp->OriginalFirstThunk));
     IMAGE_THUNK_DATA *imp_addr_table = (PIMAGE_THUNK_DATA)((BYTE*)dos_header + (imp->FirstThunk));
@@ -238,7 +156,9 @@ void il_configure_hooks(void)
       
       if (imp_name_table-> u1.ForwarderString & (1<<31)) //IMAGE_ORDINAL_FLAG32
       {
-        //sprintf(ordinal,"ordinal #%u", imp_name_table-> u1.ForwarderString & ~(1<<31));
+        sprintf(ordinal, "ordinal #%u",
+                          imp_name_table-> u1.ForwarderString & ~(1<<31)
+        );
         imp_name = &ordinal;
       }
       
@@ -247,35 +167,27 @@ void il_configure_hooks(void)
         imp_name = (BYTE*)dos_header + (imp_name_table-> u1.ForwarderString + 2);
       }
         
-      //sprintf(cosa,"  int: %s/%x, iat: %x", imp_name, (int)imp_name_table-> u1.ForwarderString, imp_addr_table-> u1.AddressOfData);
-      //MessageBoxA(0, cosa, "match", 0);
+      sprintf(debug, "  int: %s/%x, iat: %x",
+                     imp_name,
+                (int)imp_name_table-> u1.ForwarderString,
+                     imp_addr_table-> u1.AddressOfData
+      );
+      il_log(INFO, debug);
       
-      //il_log(INFO, cosa);
-      
-      if(strncmp(imp_name, "ReadFile", 8) == 0)
-      {        
-        //sprintf(cosa,"readfile | hooking iat p: %p / addr: %x  -- hook addr: %x", imp_addr_table->u1.AddressOfData, imp_addr_table->u1.AddressOfData, il_ReadFile);
-        //MessageBoxA(0, cosa, "hooking shit", 0);
-        
-        DWORD oldProtection;
-        if(VirtualProtect(&imp_addr_table->u1.AddressOfData, sizeof(DWORD), PAGE_EXECUTE_READWRITE, &oldProtection))
-        {
-           imp_addr_table->u1.AddressOfData = (DWORD)il_ReadFile;
-           //il_log(WARN,"       API function hooked!");
-        }
-        
-      }
       
       if(strncmp(imp_name, "CreateFile", 10) == 0)
       {        
-        //sprintf(cosa,"createfile | hooking iat p: %p / addr: %x  -- hook addr: %x", imp_addr_table->u1.AddressOfData, imp_addr_table->u1.AddressOfData, il_ReadFile);
-        //MessageBoxA(0, cosa, "hooking shit", 0);
+        sprintf(debug, "  |    hooking iat p: %p / addr: %x  -- hook addr: %x",
+                       imp_addr_table->u1.AddressOfData,
+                       imp_addr_table->u1.AddressOfData,
+                       il_CreateFile);
+        il_log(WARN, debug);
         
         DWORD oldProtection;
         if(VirtualProtect(&imp_addr_table->u1.AddressOfData, sizeof(DWORD), PAGE_EXECUTE_READWRITE, &oldProtection))
         {
            imp_addr_table->u1.AddressOfData = (DWORD)il_CreateFile;
-           //il_log(WARN,"       API function hooked!");
+           il_log(WARN,"  |    API function hooked!");
         }
         
       }
@@ -327,11 +239,6 @@ void il_configure_hooks(void)
     }
   }
   #endif
-  // while(1)
-  // {
-    // Sleep(2*1000);
-    // il_log(INFO, "heartbeat! <3");
-  // }
   
   return 1;
 
